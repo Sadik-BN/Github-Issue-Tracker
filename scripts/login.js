53d38bbf967813c7a073abae9bad534079c74a1e
Sadik-BN <sadikonline4@gmail.com>
2026-04-03 16:09:46 +0600

    login implemented

---
+++ scripts/login.js
@@ -0,0 +1,14 @@
+const loginBtn = document.getElementById("login-btn");
+const userField = document.getElementById("user-field");
+const passField = document.getElementById("password-field");
+loginBtn.addEventListener("click",()=>{
+    if(userField.value === "admin" && passField.value==="admin123")
+    {
+        alert("Login Successfull.");
+        window.location.replace("./home.html");
+    }
+    else
+    {
+        alert("Login Failed");
+    }
+});
